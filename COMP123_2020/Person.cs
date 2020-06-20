de7664440f2165f31fca9855b91d48a00e325f4c
Robin <rkaur750@my.centennialcollege.ca>
2020-06-19 23:19:19 -0400

    added person class

---
+++ COMP123_2020/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_2020
+{
+    class Person
+    {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // CONSTRUCTOR(S)
+        public Person(string name, int age)
+        {
+            // property initialization
+            Name = name;
+            Age = age;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+        public void SaysHello()
+        {
+            Console.WriteLine($"{Name} says Hello!");
+        }
+    }
+}
+
